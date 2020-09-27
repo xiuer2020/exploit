23a5629de0493cb2fd91e424445a8e5daa0cc290
userName <xiuer_csdn@163.com>
2020-09-27 22:55:16 +0800

    后台架构搭建成功

--- nodejs_project/merchant_management_system/server/db/connection.js
+++ nodejs_project/merchant_management_system/server/db/connection.js
@@ -1,18 +1,21 @@
-//连接mysql数据， ORM层
 const Sequelize = require('sequelize');
+//连接mysql数据， ORM层
 
-//创建连接
 module.exports = new Sequelize(config.databaseOptions.database, config.databaseOptions.user, config.databaseOptions.password, {
+  // config.databaseOptions.database： 数据库名称
+  // config.databaseOptions.user ：    用户名
+  // config.databaseOptions.password： 用户密码
 
-  //数据库地址
   host: config.databaseOptions.host,
+  //数据库地址
 
-  //连接数据库类型
   dialect: config.databaseOptions.dialect,
+  //连接数据库类型
 
-  //时区
   timezone: config.databaseOptions.timezone,
+  //时区
 
-  //设置连接池
   pool: config.databaseOptions.pool
+  //设置连接池
 })
+//创建连接
